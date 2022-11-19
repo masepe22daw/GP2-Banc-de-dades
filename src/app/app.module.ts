69ea3c9d138a14e375e9539bbf7ff69419ed19a4
magadu22daw <magadu22daw@proton.me>
2022-11-19 22:57:18 +0100

    tabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule} from '@angular/material/list';
 import { MenuComponent } from './components/menu/menu.component';
+import { MatTableModule } from '@angular/material/table' 
 import { ContingutComponent } from './components/contingut/contingut.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BolaDelMonComponent } from './components/bola-del-mon/bola-del-mon.component';
+import { TaulaComponent } from './components/taula/taula.component';
 
 
 @NgModule({
@@ -21,16 +22,20 @@ import { BolaDelMonComponent } from './components/bola-del-mon/bola-del-mon.comp
     ContingutComponent,
     SliderComponent,
     FooterComponent,
-    BolaDelMonComponent
+    BolaDelMonComponent,
+    TaulaComponent,
+    
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatTableModule,
     MatSidenavModule,
     MatListModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
