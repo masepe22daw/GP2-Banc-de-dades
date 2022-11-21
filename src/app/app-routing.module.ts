2bcc85f095e4ebd8ed7c7ad34639635dded151cb
masepe <15585133.clot@fje.edu>
2022-11-22 00:24:56 +0100

    Grafics afegits a menú

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IniciComponent } from './components/inici/inici.component';
 import { TablaDatosComponent } from './components/tabla-datos/tabla-datos.component';
+import { GraficsComponent } from './components/grafics/grafics.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'Dades',
     component: TablaDatosComponent
+  },
+  {
+    path: 'Grafics',
+    component: GraficsComponent
   }
 ];
 
